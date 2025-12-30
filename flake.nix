a54ac16db917ba7026374a0311dd0231dfc37c3f
LeahTheSlug <LeahTheSlug@protonmail.com>
2025-12-29 22:07:27 -0600

    stuffing and stuff trying to fix flake file

--- flake.nix
+++ flake.nix
@@ -1,25 +1,23 @@
-# flake.nix
 {
-	description = "Nixos config flake";
-	
-	inputs = {
-		nixpkgs.url= "github:nixos/nixpkgs/nixos-unstable";
-		
-	
+  description = "Nixos config flake";
 
-	outputs = { self, nixpkgs, ...}@inputs:
-		let
-			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
-		in
-		{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-			nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-				specialArgs = {inherit inputs;};
-				modules = [
-				./configuration.nix
-				#./kew
-				];
-			};
-		};
-}
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.leahnix = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  }
